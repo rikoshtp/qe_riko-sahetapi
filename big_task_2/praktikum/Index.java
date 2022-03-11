5324e0b6d1c5cf5b4459c9d43827a3a38d27f698
Riko Sahetapi <75010847+rikoshtp@users.noreply.github.com>
2022-03-11 23:56:01 +0700

    Create Index.java

---
+++ big_task_2/praktikum/Index.java
@@ -0,0 +1,35 @@
+//mengimport paket java.util.Scanner
+import java.util.Scanner;
+//membuat class dengan nama Tugas1_CariIndex
+public class Index {
+    //main program
+    public static void main(String[] args) {
+
+        String[][] huruf = { {"A", "B", "C/K", "D", "E"}, {"F", "G", "H", "I", "J"}, {"L", "M", "N", "0", "P"}, {"Q", "R", "S", "T", "U"}, {"V", "W", "X", "Y", "Z"}};
+
+        String inp;
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.print ("Input  : ");
+
+        inp = input.nextLine ();
+
+        boolean cek = false;
+
+        for (int i = 0; i <huruf.length; i++) {
+
+            for (int j = 0; j <huruf[0].length; j++) {
+                if(inp == huruf[i][j]){
+                    cek = true;
+                    System.out.println(inp+" Masuk Pada Indeks Ke = ["+i+"] ["+j+"]");
+                }
+            }
+        }
+
+        if(cek == false){
+
+            System.out.println("Indeks tidak ditemukan");
+        }
+    }
+}
